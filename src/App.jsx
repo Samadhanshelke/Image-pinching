8edb7dbb923733e949c57790b25042f415a4dc44
Samadhanshelke <96407937+Samadhanshelke@users.noreply.github.com>
2024-07-09 09:48:47 +0530

    img commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
 import img from './pexels-jckulkarni-910213.jpg';
 import './App.css';
-
+import {  useRef } from "react";
 function App() {
+  const imageRef = useRef(null)
+
+  const handleTouchStart = ()=>{
+    console.log('running');
+    alert("handleTouchStart")
+  }
+const handleTouchMove = ()=>{
+  console.log('moving')
+  alert("handleTouchMove")
+}
+
+const handleTouchEnd = ()=>{
+  console.log('touch end')
+  alert("handleTouchEnd")
+}
   return (
     <div className='flex justify-center items-center h-[100vh]' >
-        <div className='h-[400px] w-[400px] border-4 border-red-600    overflow-hidden'>
-          <img src={img} className='max-w-[400px] h-[400px]'  alt='Background' />
+        <div
+       
+        onTouchStart={handleTouchStart}
+        onTouchMove={handleTouchMove}
+        onTouchEnd={handleTouchEnd}
+         className='h-[400px] w-[400px] border-4 border-red-600    overflow-hidden'>
+          <img  ref={imageRef} src={img} className='max-w-[400px] h-[400px] '  alt='Background' />
         </div>
     </div>
   );
