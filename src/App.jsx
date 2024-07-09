219d8a47b0b790497e5ac9e4a1f7468919fa2673
Samadhanshelke <96407937+Samadhanshelke@users.noreply.github.com>
2024-07-09 11:39:29 +0530

    img commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,40 @@
-import img from './pexels-jckulkarni-910213.jpg';
-import './App.css';
-import {  useRef } from "react";
+import img from "./pexels-jckulkarni-910213.jpg";
+import "./App.css";
+import { useRef, useState } from "react";
 function App() {
-  const imageRef = useRef(null)
+  const [scale, setScale] = useState(1);
+  const imageRef = useRef(null);
 
-  const handleTouchStart = ()=>{
-    console.log('running');
-    alert("handleTouchStart")
-  }
-const handleTouchMove = ()=>{
-  console.log('moving')
-  alert("handleTouchMove")
-}
+  const handleTouchStart = (e) => {
+    if (e.touches.length === 2) {
+      alert('two fingers touched')
+    }
+  };
+  const handleTouchMove = () => {
+    console.log("moving");
+    // alert("handleTouchMove")
+  };
 
-const handleTouchEnd = ()=>{
-  console.log('touch end')
-  alert("handleTouchEnd")
-}
+  const handleTouchEnd = () => {
+    console.log("touch end");
+    // alert("handleTouchEnd")
+  };
   return (
-    <div className='flex justify-center items-center h-[100vh]' >
-        <div
-       
+    <div className="flex justify-center items-center h-[100vh]">
+      <div
         onTouchStart={handleTouchStart}
         onTouchMove={handleTouchMove}
         onTouchEnd={handleTouchEnd}
-         className='h-[400px] w-[400px] border-4 border-red-600    overflow-hidden'>
-          <img  ref={imageRef} src={img} className='max-w-[400px] h-[400px] '  alt='Background' />
-        </div>
+        className="h-[400px] w-[400px] border-4 border-pink-600    overflow-hidden"
+      >
+        <img
+          ref={imageRef}
+          style={{ transform: `scale(${scale})` }}
+          src={img}
+          className="max-w-[400px] h-[400px] "
+          alt="Background"
+        />
+      </div>
     </div>
   );
 }
