5a10c952476b3b68a075260927ddb338a71faa74
Samadhanshelke <96407937+Samadhanshelke@users.noreply.github.com>
2024-07-09 16:53:44 +0530

    testing3

--- src/App.jsx
+++ src/App.jsx
@@ -1,108 +1,12 @@
-import img from "./pexels-jckulkarni-910213.jpg";
-import "./App.css";
-import { useEffect, useRef, useState } from "react";
 
-function App() {
-  const [scale, setScale] = useState(1);
-  const [position, setPosition] = useState({ x: 0, y: 0 });
-
-  const imageRef = useRef(null);
-  const initialTouchDistanceRef = useRef(0);
-  const initialScaleRef = useRef(1);
-  const initialPositionRef = useRef({ x: 0, y: 0 });
-  const lastTouchPositionRef = useRef({ x: 0, y: 0 });
-
-  useEffect(() => {
-    const image = imageRef.current;
-    let touchStartDistance = 0;
-    let touchStartScale = 1;
-    let isPinching = false;
-    let isDragging = false;
-
-    const handleTouchStart = (e) => {
-      if (e.touches.length === 2) {
-        isPinching = true;
-        const touch1 = e.touches[0];
-        const touch2 = e.touches[1];
-        touchStartDistance = Math.hypot(
-          touch2.clientX - touch1.clientX,
-          touch2.clientY - touch1.clientY
-        );
-        touchStartScale = scale;
-      } else if (e.touches.length === 1) {
-        isDragging = true;
-        lastTouchPositionRef.current = {
-          x: e.touches[0].clientX,
-          y: e.touches[0].clientY,
-        };
-        initialPositionRef.current = { ...position };
-      }
-    };
-
-    const handleTouchMove = (e) => {
-      if (e.touches.length === 2 && isPinching) {
-        const touch1 = e.touches[0];
-        const touch2 = e.touches[1];
-        const currentDistance = Math.hypot(
-          touch2.clientX - touch1.clientX,
-          touch2.clientY - touch1.clientY
-        );
-
-        const scaleFactor = (currentDistance / touchStartDistance) * touchStartScale;
-
-        // Limiting the scale factor to a reasonable range (e.g., 0.5 to 3)
-        const minScale = 2;
-        const maxScale = 3;
-        const boundedScaleFactor = Math.min(Math.max(scaleFactor, minScale), maxScale);
-
-        setScale(boundedScaleFactor);
-      } else if (e.touches.length === 1 && isDragging) {
-        const deltaX = e.touches[0].clientX - lastTouchPositionRef.current.x;
-        const deltaY = e.touches[0].clientY - lastTouchPositionRef.current.y;
-
-        setPosition({
-          x: initialPositionRef.current.x + deltaX / scale,
-          y: initialPositionRef.current.y + deltaY / scale,
-        });
-
-        lastTouchPositionRef.current = {
-          x: e.touches[0].clientX,
-          y: e.touches[0].clientY,
-        };
-      }
-    };
-
-    const handleTouchEnd = () => {
-      isPinching = false;
-      isDragging = false;
-    };
-
-    image?.addEventListener("touchstart", handleTouchStart);
-    image?.addEventListener("touchmove", handleTouchMove);
-    image?.addEventListener("touchend", handleTouchEnd);
-
-    return () => {
-      image?.removeEventListener("touchstart", handleTouchStart);
-      image?.removeEventListener("touchmove", handleTouchMove);
-      image?.removeEventListener("touchend", handleTouchEnd);
-    };
-  }, [scale, position]);
-
+import ZoomableImage from './ZoomableImage';
+import img from './pexels-jckulkarni-910213.jpg'
+const App = () => {
   return (
-    <div className="flex flex-col justify-center items-center h-[100vh]">
-      <div className="h-[400px] w-[400px] border-4 border-blue-600 overflow-hidden">
-        <img
-          ref={imageRef}
-          style={{
-            transform: `scale(${scale}) translate(${position.x}px, ${position.y}px)`,
-          }}
-          src={img}
-          className="h-[400px] w-[400px]"
-          alt="Background"
-        />
-      </div>
+    <div className='overflow-hidden'>
+      <ZoomableImage src={img} />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/ZoomableImage.jsx
@@ -0,0 +1,67 @@
+import  { useRef, useState } from 'react';
+
+const ZoomableImage = ({ src }) => {
+  const imgRef = useRef(null);
+  const [zoom, setZoom] = useState(1);
+  const [initialDistance, setInitialDistance] = useState(null);
+
+  const handleTouchStart = (event) => {
+
+    if (event.touches.length === 2) {
+      const distance = getDistance(event.touches[0], event.touches[1]);
+      setInitialDistance(distance);
+    }
+  };
+
+  const handleTouchMove = (event) => {
+    console.log('first')
+    if (event.touches.length === 2) {
+      const currentDistance = getDistance(event.touches[0], event.touches[1]);
+      if (initialDistance) {
+        const scale = currentDistance / initialDistance;
+
+        if (scale > 1) {
+          console.log('Zooming in');
+          alert("Zooming in")
+        } else if (scale < 1) {
+          console.log('Zooming out');
+          alert("Zooming out")
+        }
+
+        setZoom((prevZoom) => Math.max(1, Math.min(prevZoom * scale, 3)));
+      }
+    }
+  };
+
+  const handleTouchEnd = () => {
+    setInitialDistance(null);
+  };
+
+  const getDistance = (touch1, touch2) => {
+    return Math.sqrt(
+      Math.pow(touch2.clientX - touch1.clientX, 2) + Math.pow(touch2.clientY - touch1.clientY, 2)
+    );
+  };
+
+  return (
+    <div className="flex flex-col justify-center items-center ">
+    <div className="h-[400px] w-[400px] border-4 border-blue-600 overflow-hidden">
+      <img
+        ref={imgRef}
+        onTouchStart={handleTouchStart}
+      onTouchMove={handleTouchMove}
+      onTouchEnd={handleTouchEnd}
+        style={{
+          transform: `scale(${zoom}))`,
+        }}
+        src={src}
+        className="max-w-[400px] h-[400px]"
+        alt="Background"
+      />
+    </div>
+    
+  </div>
+  );
+};
+
+export default ZoomableImage;
